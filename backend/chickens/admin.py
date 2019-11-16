ed120fa1b94a7b0b1be30c798337a5d74c3a9bd1
SangHyune546 <48785671+SangHyune546@users.noreply.github.com>
2019-11-17 01:08:56 +0900

    Update admin.py

--- backend/chickens/admin.py
+++ backend/chickens/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
+from urllib.parse import quote
+from django.utils.safestring import mark_safe
+from .models import Shop, Item, Chicken
 
 # Register your models here.
 
-from .models import Chicken
-
 admin.site.register(Chicken)
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address_link']
+    list_display_links = ['name']
+
+    def address_link(self, shop):
+        if shop.address:
+            url = 'https://map.naver.com/?query=' + quote(shop.address)
+            return mark_safe('<a href="{}" target="_blank">{}</a>'.format(url, shop.address))
+        return None
+    address_link.short_description = '주소(네이버지도)'
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['shop', 'name']
+    list_display_links = ['name']
+    list_filter = ['shop']
+    search_fields = ['name']
