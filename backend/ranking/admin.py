27dab5c721f077f17468432f52cc79f63e3cd8b4
chaehoonki <chaeefe@gmail.com>
2019-11-23 23:33:58 +0900

    update admin page

--- backend/ranking/admin.py
+++ backend/ranking/admin.py
@@ -4,6 +4,13 @@ from django.contrib import admin
 
 from .models import Ranking
 
-admin.site.register(Ranking)
-class RankingAdmin(admin.ModelAdmin):
-    list_display = ('username', 'chickenID', 'rate')
+# admin.site.register(Ranking)
+# class RankingAdmin(admin.ModelAdmin):
+#     list_display = ('username', 'chickenID', 'rate')
+
+
+@admin.register(Ranking)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['username', 'chickenID', 'rate']
+    list_display_links = ['username']
+    search_fields = ['username']   
